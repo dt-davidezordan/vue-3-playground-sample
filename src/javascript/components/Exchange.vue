5a962c702201f51429b4169977b379acd67b9798
dt-davidezordan <80255033+dt-davidezordan@users.noreply.github.com>
2021-03-12 16:42:05 +0000

    Initial commit

---
+++ src/javascript/components/Exchange.vue
@@ -0,0 +1,20 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  async setup() {
+    const response = await fetch('https://api.exchangeratesapi.io/latest');
+    const exchangeRates = await response.json();
+
+    return { rates: exchangeRates.rates };
+  }
+});
+</script>
+
+<template>
+<ul class="list-group">
+  <li class="list-group-item">Change to USD: {{ rates.USD }}</li>
+  <li class="list-group-item">Change to GBP: {{ rates.GBP }} </li>
+</ul>
+</template>
+
---
+++ src/typescript/components/Exchange.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+interface ApiResponse {
+  rates: {
+    USD: string,
+    GBP: string
+  }
+}
+
+export default defineComponent({
+  async setup() {
+    const response = await fetch('https://api.exchangeratesapi.io/latest');
+    const exchangeRates: ApiResponse = await response.json();
+
+    return { rates: exchangeRates.rates };
+  }
+});
+</script>
+
+<template>
+<ul class="list-group">
+  <li class="list-group-item">Change to USD: {{ rates.USD }}</li>
+  <li class="list-group-item">Change to GBP: {{ rates.GBP }} </li>
+</ul>
+</template>
+
